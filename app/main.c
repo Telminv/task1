501e10dfd44297ea9e89a175a35bfeeeba2b789a
Telminv <50484487+Telminv@users.noreply.github.com>
2019-05-18 21:13:54 +0300

    Update main.c

--- app/main.c
+++ app/main.c
@@ -4,15 +4,10 @@
 int main()
 {
     double a,b,c;
-    printf("enter a\n");
-    scanf("%lf",&a);
-    printf("%lf\n",a);
-    printf("enter b\n");
-    scanf("%lf",&b);
-    printf("%lf\n",b);
-    printf("enter c\n");
-    scanf("%lf",&c);
-    printf("%lf\n",c);
+do{
+    g = scanf ("%lf%lf%lf",&a, &b, &c);
+    }
+    while (g != 3);
     myfunc(a,b,c);
     return 0;
 }
